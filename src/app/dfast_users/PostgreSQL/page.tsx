04c5e527220aa585c84669e102df0a1ffa008227
katawutp <katawut.paramee@gmail.com.com>
2025-01-19 15:06:38 +0700

    UserDatabase

---
+++ src/app/dfast_users/PostgreSQL/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+const Page = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/dfast_users");
+      const json = await res.json();
+      setData(json);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>DFast_Users Data</h1>
+      <table>
+        <thead>
+          <tr>
+            {data.length > 0 &&
+              Object.keys(data[0]).map((key) => <th key={key}>{key}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map((value, idx) => (
+                <td key={idx}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/dfast_users/page.tsx
@@ -0,0 +1,58 @@
+// app/page.tsx
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+
+const Page: React.FC = () => {
+  const [csvData, setCsvData] = useState<string[][]>([]);
+
+  useEffect(() => {
+    // Fetch the CSV file
+    fetch('/dfast_users.csv')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch CSV file');
+        }
+        return response.text();
+      })
+      .then((data) => {
+        // Parse the CSV file
+        const parsedData = Papa.parse<string[]>(data, { header: false });
+        setCsvData(parsedData.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching or parsing CSV:', error);
+      });
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", marginTop: "20px", justifyContent: "center", alignItems: "center" }}>
+      <h1 style={{ fontWeight: "bold", margin: "10px", justifyContent: 'center', alignItems: 'center' }}>DFast Users</h1>
+      {csvData.length > 0 ? (
+        <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+          <thead>
+            <tr>
+              {csvData[0].map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {csvData.slice(1).map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Page;
