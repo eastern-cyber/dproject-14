16eac585edb93131f14297aeca2357578b5d7cf4
katawutp <katawut.paramee@gmail.com.com>
2025-02-10 14:07:58 +0700

    dproject_users_20250210_1356a

--- src/app/dproject_users/page.tsx
+++ src/app/dproject_users/page.tsx
@@ -9,7 +9,7 @@ const Page: React.FC = () => {
 
   useEffect(() => {
     // Fetch the CSV file
-    fetch('/dproject_users_20250202_1346.csv')
+    fetch('/dproject_users_20250210_1356a.csv')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Failed to fetch CSV file');
