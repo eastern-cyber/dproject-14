06ecb799192f022acfa0becab958e3f7c94c8e45
katawutp <katawut.paramee@gmail.com.com>
2025-02-12 16:17:57 +0700

    <ConnectButton>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,10 +97,49 @@ export default function Home() {
         border: "1px solid #333",
         borderRadius: "8px",
       }}>
-        <ConnectButton locale={"en_US"}
-          client={client}
-          chain={chain}
-        />
+        <ConnectButton locale={"en_US"} 
+                        client={client}
+                        chain={chain}
+                        wallets={[ inAppWallet ({
+                        auth: {
+                            options: [
+                                "email",
+                            ]
+                            }
+                        }) ]}
+                        connectModal={{
+                            title: "เชื่อมต่อกระเป๋า",
+                            titleIcon: "https://dfi.fund/_next/static/media/DFastLogo_650x600.4f2ec315.svg",
+                            size: "wide", // Change to "compact" or "auto" 
+                        }}
+                        supportedTokens={{
+                        [chain.id]: [
+                            {
+                                address: "0xca23b56486035e14F344d6eb591DC27274AF3F47",
+                                name: "DProject",
+                                symbol: "DFI",
+                                icon: "https://dfi.fund/_next/static/media/DFastLogo_650x600.4f2ec315.svg",
+                            },
+                            {
+                                address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+                                name: "USDC",
+                                symbol: "USDC",
+                                icon: "https://polygonscan.com/token/images/centre-usdc_32.png",
+                            },
+                            {
+                                address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+                                name: "USDT",
+                                symbol: "USDT",
+                                icon: "https://polygonscan.com/token/images/tether_32.png",
+                                },
+                        ],
+                        }}
+                        supportedNFTs={{
+                        [chain.id]: [
+                            "0x2a61627c3457cCEA35482cAdEC698C7360fFB9F2", // nft contract address
+                        ],
+                        }}
+                    />
         {contractMetadata && (
           <div style={{
             display: "flex",
