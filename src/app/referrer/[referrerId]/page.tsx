85325b7b5f82722a0e3fa9a8180af29fc4d9001e
katawutp <katawut.paramee@gmail.com.com>
2025-02-12 22:53:29 +0700

    Logo

--- src/app/referrer/[referrerId]/page.tsx
+++ src/app/referrer/[referrerId]/page.tsx
@@ -1,27 +1,33 @@
 "use client";
+import React from 'react'
+import Image from "next/image";
+import dprojectIcon from "@public/DFastLogo_650x600.svg";
 import { chain } from '@/app/chain';
 import { client } from '@/app/client';
-import React from 'react'
-// import { getContractMetadata } from 'thirdweb/extensions/common';
 import { ConnectButton, useActiveAccount, useReadContract } from 'thirdweb/react';
 import { inAppWallet } from 'thirdweb/wallets';
-// import { contract } from '../../../../utils/contracts';
 
 export default function page({ 
     params,
  }: {
     params: { referrerId: string };
 }) {
-
-    // const account = useActiveAccount();
     
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex flex-col items-center">
-            <div className="flex flex-col items-center justify-center p-10 m-5 border border-gray-800 rounded-lg">
-                <h1 className="p-4 text-1xl md:text-3xl text-2xl font-semibold md:font-bold tracking-tighter">
+            <div className="flex flex-col items-center justify-center p-10 m-5 border border-gray-800 rounded-lg">                
+                <Image
+                src={dprojectIcon}
+                alt=""
+                className="mb-4 size-[100px] md:size-[100px]"
+                style={{
+                    filter: "drop-shadow(0px 0px 24px #a726a9a8"
+                }}
+                />
+                <h1 className="p-4 md:text-2xl text-2xl font-semibold md:font-bold tracking-tighter">
                     สมัครใช้งาน
                 </h1>
-                <div className="flex justify-center mb-2">
+                <div className="flex justify-center m-5">
                     <ConnectButton locale={"en_US"} 
                         client={client}
                         chain={chain}
@@ -66,10 +72,19 @@ export default function page({
                         }}
                     />
                 </div>
-                <div>
+                <div className="flex flex-col items-center justify-center p-5 m-5">
                     {/* <p>ผู้แนะนำ: sunyapakorn.1958@gmail.com</p>*/}
-                    <p style={{fontSize: "19px"}}><b>เลขที่กระเป๋าของผู้แนะนำ</b></p>
-                        <div style={{border: "1px solid #666", background: "#222", padding: "0px 6px", margin: "10px"}}>
+                    <p className="flex flex-col items-center justify-center text-[20px] m-5"><b>เลขที่กระเป๋าของผู้แนะนำ</b></p>
+                    <div style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center", // Centers vertically
+                        border: "1px solid #666",
+                        background: "#222",
+                        padding: "0px 6px",
+                        margin: "10px",
+                        height: "40px" // Optional: Ensure enough height for centering
+                    }}>
                         {/* <p style={{fontSize: "18px"}}>{params.referrerId}</p> */}
                         <p style={{ fontSize: "18px" }}>
                             {params.referrerId ? `${params.referrerId.slice(0, 6)}...${params.referrerId.slice(-4)}` : ""}
@@ -77,65 +92,9 @@ export default function page({
                         </div>
                 </div>
                 <div className="flex flex-col items-center mb-6">
-                    {/* <WalletPublicKey walletAddress={account?.address || ""}/> */}
                 </div>
 
-            </div>
-            <div className="flex flex-col items-center">
-                    <a 
-                        className="flex flex-col mt-8 border border-zinc-500 px-4 py-3 rounded-lg hover:bg-zinc-800 transition-colors hover:border-zinc-800"
-                        href="/">กลับหน้าหลัก</a>
             </div>
         </main>
   )
 }
-
-// type walletAddresssProps = {
-//     walletAddress?: string;
-// };
-
-// const WalletPublicKey: React.FC<walletAddresssProps> = ({ walletAddress }) => {
-
-//     const account = useActiveAccount();
-
-//     const { data: contractMetadata } = useReadContract(
-//         getContractMetadata,
-//         {
-//           contract: contract,
-//         }
-//       );
-
-//     return (
-//         <div style={{
-//             display: "flex",
-//             flexDirection: "column",
-//             width: "100%",
-//             alignItems: "center",
-//             justifyContent: "center",
-//           }}>
-//             <div 
-//             style={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 alignItems: "center",
-//                 width: "100%",
-//                 fontSize: "24px",
-//                 justifyContent: "center",
-//                 paddingTop: "15px",
-//                 paddingBottom: "5px",
-//               }}
-//             >
-//                 <span className="mt-4 text-[22px]">ลิ้งค์แนะนำของท่าน</span>
-//                 <div style={{border: "1px solid #666", background: "#222", padding: "4px 8px", margin: "6px"}}>
-//                 <p className="text-[16px] break-all">{walletAddress ? `https://dfi.fund/referrer/${walletAddress}` : "ยังไม่ได้เชื่อมกระเป๋า !"} </p>    
-//                 </div>
-//                 <div>
-//                 {/* <p className="text-[16px] break-all">{walletAddress ? walletAddress || "" : "ยังไม่ได้เชื่อมกระเป๋า !"} </p> */}
-//                 </div>
-//                 <div className="flex flex-col items-center justify-center p-5 border border-gray-800 rounded-lg text-lg text-center font-bold mt-10">
-//                     เตรียมเปิดร้านค้าออนไลน์<br />และประชาสัมพันธ์ผ่านแอ๊พ <span className="mt-2 text-[#eb1c24] text-3xl animate-blink">ก๊อกๆๆ !</span>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// };
