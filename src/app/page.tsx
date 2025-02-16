876de9a46ad41d162a2ed0434a7656f3b58e8c24
katawutp <katawut.paramee@gmail.com.com>
2025-02-16 07:57:02 +0700

    pate.tsx ล็อกอิน

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
                   ]
                   }
               }) ]}
+              connectButton={{ label: "ล็อกอิน" }}
               connectModal={{
                   title: "เชื่อมต่อกระเป๋า",
                   titleIcon: "https://dfi.fund/_next/static/media/DFastLogo_650x600.4f2ec315.svg",
