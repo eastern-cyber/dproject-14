3ff9eb76a37da4a8fbc116766a5c73878ce9c0eb
katawutp <katawut.paramee@gmail.com.com>
2025-03-26 21:20:12 +0700

    system update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ export default function NotFound() {
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex flex-col items-center">
     <div className="flex flex-col items-center justify-center p-10 m-5 border border-gray-800 rounded-lg">                
-        <p>This is a restricted area</p>
-        <p>ท่านไม่สามารถใช้งานพื้นที่นี้ได้</p>
+        <p>Please wait for system update to Plan B System</p>
+        <p>ปิดระบบชั่วคราวเพื่อเตรียมรองรับแผน B</p>
         <div className="flex flex-col items-center">
             <Link 
                 className="flex flex-col mt-8 border border-zinc-500 px-4 py-3 rounded-lg hover:bg-zinc-800 transition-colors hover:border-zinc-800"
